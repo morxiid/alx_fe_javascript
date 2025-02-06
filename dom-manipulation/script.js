00cc5eae8f336aa18f6a4653031d8a1b63ed0046
Morchiid Aymane <morxiidbullishly@gmail.com>
2025-02-06 21:14:33 +0100

    fixing html inputs

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -27,13 +27,6 @@ document.getElementById("newQuote").addEventListener("click", showRandomQuote);
 // Function to create the quote addition form dynamically
 function createAddQuoteForm() {
     const formContainer = document.createElement("div");
-    formContainer.innerHTML = `
-        <input id="newQuoteText" type="text" placeholder="Enter a new quote" />
-        <input id="newQuoteCategory" type="text" placeholder="Enter quote category" />
-        <button id="addQuoteButton">Add Quote</button>
-        <button id="exportButton">Export Quotes</button>
-        <input type="file" id="importFile" accept=".json" onchange="importFromJsonFile(event)" />
-    `;
     document.body.appendChild(formContainer);
     document.getElementById("addQuoteButton").addEventListener("click", addQuote);
     document.getElementById("exportButton").addEventListener("click", exportToJsonFile);
